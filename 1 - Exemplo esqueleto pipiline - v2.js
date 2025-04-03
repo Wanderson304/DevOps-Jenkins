ad148d4b4eef7df3cf3db1c4b2e0bba6323086fe
Wanderson Silva <ca-scudo@hotmail.com>
2025-04-03 01:10:02 -0300

    Update 1 - Exemplo esqueleto pipiline - v2.js

--- 1 - Exemplo esqueleto pipiline - v2.js
+++ 1 - Exemplo esqueleto pipiline - v2.js
@@ -1,3 +1,4 @@
 //Exemplo de um arquivo jenkisfile
 //A extenção do arquivo não é .js coloquei apenas para ter uma formatação minima.
-//Aqui usamos os Plugins 
+//Aqui usamos os Plugins do Docker e Kubernetes instalado no Jenkins
+
