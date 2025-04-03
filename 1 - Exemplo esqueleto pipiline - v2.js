b1b66d80a1e63b2e1e7901076cde1ef3efaf2b1a
Wanderson Silva <ca-scudo@hotmail.com>
2025-04-03 01:34:52 -0300

    Update 1 - Exemplo esqueleto pipiline - v2.js

--- 1 - Exemplo esqueleto pipiline - v2.js
+++ 1 - Exemplo esqueleto pipiline - v2.js
@@ -2,3 +2,37 @@
 //A extenção do arquivo não é .js coloquei apenas para ter uma formatação minima.
 //Aqui usamos os Plugins do Docker e Kubernetes instalado no Jenkins
 
+pipeline {
+    agent any
+
+    stages {
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    //Variável de construção da imagem Docker
+                    dockerapp = docker.build("Wanderson304/guia-pratico-jenkins:${env.BUILD_ID}", '-f ./src/Dockerfile ./src')
+                }
+            }
+        }
+
+        stage('Push Docker Image') {
+            steps {
+                script {
+                    //Criando a conexão com o Docker Hub para power enviar a imagem criada no Step anterior
+                    docker.withRegistry('https://registry.hub.docker.com', 'dockerhub') {
+                    //Enviar a imagem dockerapp para o Docker Hub
+                    dockerapp.push('latest')
+                    dockerapp.push("${env.BUILD_ID}")
+                    }
+                }
+            }
+        }
+        
+        stage('Deploy no Kubernetes') {
+            steps {
+                sh 'echo "Executando o comando Docker kubectl apply"'
+            }
+        }
+    }
+}
+
