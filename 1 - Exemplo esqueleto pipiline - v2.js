0f79ecf72643c29feb121b23dd124785d520000e
Wanderson Silva <ca-scudo@hotmail.com>
2025-04-03 01:59:29 -0300

    Update 1 - Exemplo esqueleto pipiline - v2.js

--- 1 - Exemplo esqueleto pipiline - v2.js
+++ 1 - Exemplo esqueleto pipiline - v2.js
@@ -20,7 +20,7 @@ pipeline {
                 script {
                     //Criando a conexão com o Docker Hub para power enviar a imagem criada no Step anterior
                     docker.withRegistry('https://registry.hub.docker.com', 'dockerhub') {
-                    //Enviar a imagem dockerapp para o Docker Hub
+                    //Enviar a imagem dockerapp para o Docker Hub.
                     dockerapp.push('latest')
                     dockerapp.push("${env.BUILD_ID}")
                     }
@@ -35,4 +35,5 @@ pipeline {
         }
     }
 }
+
 
