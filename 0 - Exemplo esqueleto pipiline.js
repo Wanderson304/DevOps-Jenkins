6c516406f3cbd7aff02daae0161230434a6b3d79
Wanderson Silva <ca-scudo@hotmail.com>
2025-04-03 00:33:44 -0300

    Update 0 - Exemplo esqueleto pipiline.js

--- 0 - Exemplo esqueleto pipiline.js
+++ 0 - Exemplo esqueleto pipiline.js
@@ -10,11 +10,13 @@ pipeline {
                 sh 'echo "Executando o comando Docker Build"'
             }
         }
+        
         stage('Build Docker Image') {
             steps {
                 sh 'echo "Executando o comando Docker push"'
             }
         }
+        
         stage('Build Docker Image') {
             steps {
                 sh 'echo "Executando o comando Docker kubectl apply"'
